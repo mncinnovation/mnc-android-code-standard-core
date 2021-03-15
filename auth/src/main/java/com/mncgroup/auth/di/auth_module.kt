e263fed2538f80e8c7c995c9279afbecafc7ca99
bayu <bayu.wijaya@mncgroup.com>
2021-03-15 15:13:18 +0700

    Initial di for auth LoginViewModel.kt

--- auth/src/main/java/com/mncgroup/auth/di/auth_module.kt
+++ auth/src/main/java/com/mncgroup/auth/di/auth_module.kt
@@ -2,11 +2,22 @@ package com.mncgroup.auth.di
 
 import com.mncgroup.auth.repository.AUTH_PATH
 import com.mncgroup.auth.repository.AuthApi
+import com.mncgroup.auth.repository.AuthRepositoryImpl
+import com.mncgroup.auth.ui.login.LoginViewModel
 import com.mncgroup.core.network.createApi
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val authRemoteDataSourceModule = module {
     single { createApi<AuthApi>(AUTH_PATH, get(), getProperty("base_url")) }
 }
 
-val authModule = listOf(authRemoteDataSourceModule)
+val authRepository = module {
+    single { AuthRepositoryImpl(get()) }
+}
+
+val authFeatures = module {
+    viewModel { LoginViewModel(get()) }
+}
+
+val authModule = listOf(authRemoteDataSourceModule, authRepository, authFeatures)
--- auth/src/main/java/com/mncgroup/auth/ui/login/LoginViewModel.kt
+++ auth/src/main/java/com/mncgroup/auth/ui/login/LoginViewModel.kt
@@ -1,16 +1,20 @@
 package com.mncgroup.auth.ui.login
 
+import com.mncgroup.auth.model.LoginRequest
 import com.mncgroup.auth.repository.AuthRepository
 import com.mncgroup.core.ui.BaseViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-class LoginViewModel(val repository: AuthRepository) : BaseViewModel() {
+class LoginViewModel(private val repository: AuthRepository) : BaseViewModel() {
 
-    fun authLogin(){
+    fun authLogin(email: String, password : String){
         GlobalScope.launch(Dispatchers.Main){
-
+            val result = repository.requestLogin(LoginRequest(email, password))
+            result.handle {
+                //handle result ok
+            }
         }
     }
 
